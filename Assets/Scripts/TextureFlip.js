fdb27bdaa99f88b620a1a0067e64f819f3b83f80
lorencs <mikuslorence@gmail.com>
2013-09-20 13:53:24 -0600

    character texture chagnes direction when moving around
    - tutorial 3

---
+++ Assets/Scripts/TextureFlip.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var X : float;
+
+function Start () {
+	X = transform.localScale.x;
+}
+
+function Update () {
+	if (Input.GetKey("a")){
+	print("a is pressed");
+		transform.localScale.x = X;
+	} else if(Input.GetKey("d")){
+	print("d is pressed");
+		transform.localScale.x = -X;
+	}
+}
