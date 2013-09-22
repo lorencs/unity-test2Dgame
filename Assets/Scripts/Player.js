d8f3fe0b45b65aeeb268b7f886094a71c2fce79e
Mikus Lorencs <mikuslorence@gmail.com>
2013-09-21 20:40:37 -0600

    moving animation + refactoring

---
+++ Assets/Scripts/Player.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var moving : boolean;
+var direction : int;
+var jumping : boolean;
+
+var X : float;
+var AT : AnimateTexture;
+
+function Start () {
+	direction = -1;
+	X = transform.localScale.x;
+	
+}
+
+function Update () {
+	//handle input
+	if (Input.GetKey("a") || Input.GetKey("left")){
+		moving = true;
+		direction = -1;
+		transform.localScale.x = X;
+	} else if(Input.GetKey("d") || Input.GetKey("right")){
+		moving = true;
+		direction = 1;
+		transform.localScale.x = -X;
+	} else {
+		moving = false;
+	}
+	
+	AT = GetComponent(AnimateTexture);
+	//handle animation/sprites
+	if (moving)
+		AT.rowNumber = 1;
+	else
+		AT.rowNumber = 0;	
+}
